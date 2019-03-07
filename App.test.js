d63c61d2d8292713cb6b83dcde6fdd32bde44951
isabelcooper <isabel@triptease.com>
2019-03-07 09:00:14 +0000

    jest setup

---
+++ App.test.js
@@ -0,0 +1,20 @@
+// describe('Google Test', () => {
+//     beforeAll(async () => {
+//         await page.goto('https://google.com');
+//     });
+//
+//     it('should be titled "Google"', async () => {
+//         await expect(page.title()).resolves.toMatch('Google');
+//     });
+// });
+// describe('Launching home', () => {
+//
+//     beforeAll(async() => {
+//         await page.goto('http://localhost:5432');
+//     })
+//
+//     it('shows app title on page load', async() => {
+//         await expect(page).toMatch("April Fool");
+//     });
+//
+// });
