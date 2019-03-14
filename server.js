6946b71b4cd18552e0a7af2ab1dc11ec0d7cd596
isabelcooper <isabel@triptease.com>
2019-03-14 20:31:56 +0000

    testing heroku commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const sender = require('./routes/sender');
 
 // app.use(express.static(__dirname + '/public'));
